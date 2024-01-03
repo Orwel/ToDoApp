3d753463378aa0273f35d50c49759e7214c85c7a
Orwel <jdvanegas@hotmail.com>
2024-01-03 17:14:59 -0500

    Funciones todobutton y todosearch en eventos y estados

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -4,7 +4,16 @@ function CreateTodoButton(){
       <div className="create-todo-section1">
         <form className="create-todo-section2">
           <input type="text" placeholder="Ingresa una tarea" name="taskInput" />
-          <button type="submit">Agregar Tarea</button>
+          <button 
+          type="submit"
+          className='CreateTodoButton'
+          onClick={
+            (event) => {
+              console.log('le diste click')
+              console.log(event.target)
+            }
+          }
+          >Agregar Tarea</button>
         </form>
       </div>
     );
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,23 @@
+import React from "react";  
+
+
 function TodoSearch(){
+  const [ searchValue, setSearchValue ] = React.
+  useState('');
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
+
     return (
-      <input placeholder="Cortar cebolla"></input>
+      <input
+        placeholder="Cortar cebolla"
+        className="Cortar cebolla"
+        value={searchValue}
+        onChange={(event) => {
+          setSearchValue(event.target.value)
+
+        }}
+      ></input>
     );
   }
 
